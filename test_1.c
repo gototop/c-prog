9e5e9bda05433e4532af72b6bf49baee5e1e6a46
wcy <asfdsdt123ygq@gmail.com>
2014-03-29 11:43:53 +0800

    wrong example: invalid array boundary

---
+++ test_1.c
@@ -0,0 +1,35 @@
+int a[10]={1 ,2, 3, 4, 5, 55, 8512, 151, 6161, 879};
+int num = sizeof(a) / sizeof(a[0]);
+void swap(int *_a, int *_b)
+{
+	int c;	
+	int *tem = &c;
+	
+	*tem = *_a;
+	*_a = *_b;
+	*_b = *tem;	
+}
+
+int main()
+{
+	int i, j, tem;
+	
+	for(j=0;j<num-1;j++)
+	{
+		for(i=0;i<num;i++)
+		{			
+			if(a[i]>a[i+1])
+			{
+				swap(&a[i], &a[i+1]);
+			}
+		}
+	}
+
+	for(i=0;i<num;i++)
+	{
+		printf("%d ", a[i]);
+	}	
+
+	printf("\n");
+	return 0;
+}
